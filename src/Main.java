c5322a0f0f07beac84e0d5d70790dfe0fb0f0d5b
Stas Git <yolkhingp@gmail.com>
2024-12-15 19:20:49 +0300

    Task2

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+        int balance = 200;
+        int topUp = 300;
+
+        int bonus;
+        if (topUp > 1000) {
+            bonus = topUp / 100;
+        } else {
+            bonus = 0;
+        }
+        int finaleBalance = balance + topUp + bonus;
+        System.out.println("Бонус за мили: " + bonus);
+        System.out.println("Итоговый счёт: " + finaleBalance);
+
+    }
+
+
+}
